53e19b3215ceb3b266ed7f0d51def194d1f27189
RiteshGadre <riteshgadre9@gmail.com>
2025-07-03 14:44:08 +0530

    Day 3 Completed

--- Day-3/server.js
+++ Day-3/server.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+const notes = [];
+
 app.get('/home', (req, res) => {
     res.send('Welcome to home page');
+})
+
+app.get('/about', (req, res) => {
+    res.send('Welcome to about page');
+})
+
+app.post('/notes', (req, res) => {
+    console.log(req.body)
+    notes.push(req.body);
+    res.status(200).send({
+        title : "Noted has been added",
+        notes: notes
+    })
 })
 
 app.listen(3000, ()=> {
